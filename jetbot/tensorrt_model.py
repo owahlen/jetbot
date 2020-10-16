aa5438ef8da72d1b65706566316389342acc379f
Oliver Wahlen <oliver.wahlen@infinit-group.de>
2020-10-16 23:21:38 +0200

    TRTModel can execute in a different thread

--- jetbot/tensorrt_model.py
+++ jetbot/tensorrt_model.py
@@ -1,59 +1,73 @@
-import atexit
-
 import numpy as np
-# noinspection PyUnresolvedReferences
-import pycuda.autoinit  # this import initializes pycuda
 import pycuda.driver as cuda
 import tensorrt as trt
+from pycuda.tools import make_default_context
 
 
 class TRTModel(object):
 
     def __init__(self, engine_path, final_shapes=None):
-        # initialize runtime
-        self.logger = trt.Logger(trt.Logger.WARNING)
-        self.runtime = trt.Runtime(self.logger)
+        """Initialize TensorRT engine and context."""
+        cuda.init()
+        self.cuda_context = make_default_context()
 
         # load engine
-        with open(engine_path, 'rb') as f:
-            self.engine = self.runtime.deserialize_cuda_engine(f.read())
-        self._create_buffers()
-        self.context = self.engine.create_execution_context()
+        self.logger = trt.Logger(trt.Logger.INFO)
+        self.engine = self._load_engine(engine_path)
 
         # allow override for reshaping each output
         self.final_shapes = final_shapes
 
-        # destroy at exit
-        atexit.register(self.destroy)
+        try:
+            self.context = self.engine.create_execution_context()
+            self.stream = cuda.Stream()
+            self.host_inputs, self.host_outputs, self.cuda_inputs, self.cuda_outputs, \
+            self.bindings, self.input_names, self.output_names = self._allocate_buffers()
+        except Exception as e:
+            raise RuntimeError('failed to allocate CUDA resources') from e
+        finally:
+            self.cuda_context.pop()
 
-    def _create_buffers(self):
-        self.host_inputs = []  # raveled data of each input as passed to the self.execute method
-        self.cuda_inputs = []  # host_inputs copied to cuda device
-        self.cuda_outputs = []  # outputs on cuda device
-        self.host_outputs = []  # raveled cuda_outputs copied back to host
-        self.input_names = []  # names of all input bindings
-        self.output_names = []  # names of all output bindings
-        self.bindings = [] # pointers to allocated cuda memory
-        self.stream = cuda.Stream()
+    def __del__(self):
+        """Free CUDA memory and context."""
+        del self.cuda_outputs
+        del self.cuda_inputs
+        del self.stream
+        self.runtime.__del__()
+        self.context.__del__()
+        self.engine.__del__()
+        del self.logger
 
+    def _load_engine(self, engine_path):
+        logger = trt.Logger(trt.Logger.INFO)
+        self.runtime = trt.Runtime(logger)
+        with open(engine_path, 'rb') as f:
+            return self.runtime.deserialize_cuda_engine(f.read())
+
+    def _allocate_buffers(self):
+        host_inputs, host_outputs, cuda_inputs, cuda_outputs, bindings, input_names, output_names = \
+            [], [], [], [], [], [], []
         for binding in self.engine:
             size = trt.volume(self.engine.get_binding_shape(binding)) * self.engine.max_batch_size
             nptype = trt.nptype(self.engine.get_binding_dtype(binding))
             host_mem = cuda.pagelocked_empty(size, nptype)
             cuda_mem = cuda.mem_alloc(host_mem.nbytes)
-
-            self.bindings.append(int(cuda_mem))
+            bindings.append(int(cuda_mem))
             if self.engine.binding_is_input(binding):
-                self.host_inputs.append(host_mem)
-                self.cuda_inputs.append(cuda_mem)
-                self.input_names.append(binding)
+                host_inputs.append(host_mem)
+                cuda_inputs.append(cuda_mem)
+                input_names.append(binding)
             else:
-                self.host_outputs.append(host_mem)
-                self.cuda_outputs.append(cuda_mem)
-                self.output_names.append(binding)
-        self.outputs = [None] * len(self.host_outputs)
+                host_outputs.append(host_mem)
+                cuda_outputs.append(cuda_mem)
+                output_names.append(binding)
+        return host_inputs, host_outputs, cuda_inputs, cuda_outputs, bindings, input_names, output_names
 
     def execute(self, *inputs):
+        """Detect objects in the input image(s)."""
+
+        self.cuda_context.push()
+
         # copy inputs from host to device
         for i in range(len(self.host_inputs)):
             np.copyto(self.host_inputs[i], inputs[i].ravel())
@@ -63,6 +77,7 @@ class TRTModel(object):
         self.context.execute_async(bindings=self.bindings, stream_handle=self.stream.handle)
 
         # copy outputs from cuda device back to host and reshape them as needed
+        outputs = [None] * len(self.host_outputs)
         for i in range(len(self.host_outputs)):
             cuda.memcpy_dtoh_async(self.host_outputs[i], self.cuda_outputs[i], self.stream)
             if self.final_shapes is not None:
@@ -70,17 +85,12 @@ class TRTModel(object):
             else:
                 output_name = self.output_names[i]
                 shape = self.engine.get_binding_shape(output_name)
-            self.outputs[i] = self.host_outputs[i].reshape(shape)
+            outputs[i] = self.host_outputs[i].reshape(shape)
 
         # synchronize async execution
         self.stream.synchronize()
-        return self.outputs
+        self.cuda_context.pop()
+        return outputs
 
     def __call__(self, *inputs):
         return self.execute(*inputs)
-
-    def destroy(self):
-        self.runtime.__del__()
-        self.context.__del__()
-        self.engine.__del__()
-        del self.logger
